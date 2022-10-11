bf6fb19980536c1934fa862aa7eef27a0cd3ac19
Tamulus47 <mahmoud.mostafa0047@gmail.com>
2022-10-11 04:01:53 +0200

    add Products endpoint

---
+++ src/models/Products.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from 'express';
+import { Product,Products } from '../handlers/Products';
+
+const product= new Products;
+
+const index = async (_req: Request, res: Response) => {
+    const result = await product.index()
+    res.json(result)
+  }
+
+  const show = async (req: Request, res: Response) => {
+    try {
+      const id = Number(req.params.id);
+      const result = await product.show(id);
+      res.json(result);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
+
+  const create = async (req: Request, res: Response) => {
+    try {
+      const { name, price } = req.body;
+      const p: Product = { name, price };
+      const result = await product.create(p);
+      res.json(result);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
+
+  export const product_route= (app: express.Application)=>{
+    app.get('/Products', index)
+    app.get('/Products/:id', show)
+    app.post('/Products', create)
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import { product_route } from './models/Products' 
 
 const app: express.Application = express()
 const address: string = "127.0.0.1:8080"
@@ -12,3 +13,5 @@ app.get('/', function (_req: Request, res: Response) {
 app.listen(8080, function () {
     console.log(`starting app on: ${address}`)
 })
+
+product_route(app)
