15ebc23e930aeca5bc1e3979c62aac608b225211
Tamulus47 <mahmoud.mostafa0047@gmail.com>
2022-10-17 06:17:12 +0200

    edit

--- src/helpers/JWT-helper.ts
+++ src/helpers/JWT-helper.ts
@@ -8,14 +8,14 @@ export function gen_token(id: number) {
 }
 
 export function verify_auth(req: Request, id?: number){
-    const authorization = req.headers.authorization;
-    if(authorization){
-        const JWT= authorization.replace('Bearer ','')
+    const auth_token = req.headers.authorization;
+    if(auth_token){
+        const JWT= auth_token.replace('Bearer ','')
         const JWTP = verify(JWT as string, SECRET) as JwtPayload;
         if (id && JWTP.user.id != id) {
             throw new Error("can't show this data for this user");
         }
     }else{
-    throw new Error('authorization not provided')
+    throw new Error('authorization token not provided')
     }
 }
